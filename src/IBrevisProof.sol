7aba062afd3b3b3c0d61972425f9b09c0176f41f
Junda Liu <517443+i9@users.noreply.github.com>
2025-02-25 21:56:39 -0800

    update campaign, handle zk proof, no merkle tree needed

---
+++ src/IBrevisProof.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+interface IBrevisProof {
+    struct ProofData {
+        bytes32 commitHash;
+        bytes32 vkHash;
+        bytes32 appCommitHash; // zk-program computing circuit commit hash
+        bytes32 appVkHash; // zk-program computing circuit Verify Key hash
+        bytes32 smtRoot;
+    }
+    function submitProof(
+        uint64 _chainId,
+        bytes calldata _proofWithPubInputs
+    ) external returns (bytes32 requestId, bytes32 appCommitHash, bytes32 appVkHash);
+    
+    function submitAggProof(
+        uint64 _chainId,
+        bytes32[] calldata _requestIds,
+        bytes calldata _proofWithPubInputs
+    ) external;
+
+    function validateAggProofData(uint64 _chainId, ProofData[] calldata _proofDataArray) external view;
+}
