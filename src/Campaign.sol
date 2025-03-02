a3b8da526e1321e070714bf4c70f89ba0ced68a6
Junda Liu <517443+i9@users.noreply.github.com>
2025-03-01 20:20:18 -0800

    good w/o alm

--- src/Campaign.sol
+++ src/Campaign.sol
@@ -3,45 +3,39 @@ pragma solidity ^0.8.20;
 
 import "./IBrevisProof.sol";
 import "./Ownable.sol";
-import {IERC20} from "forge-std/interfaces/IERC20.sol";
+import "./Rewards.sol";
 
-struct AddrAmt {
-    address token;
-    uint256 amount;
-}
+import {IERC20} from "forge-std/interfaces/IERC20.sol";
 
 struct Config {
     address creator;
     uint64 startTime;
     uint32 duration; // how many seconds this campaign is active, end after startTime+duration
-    AddrAmt[] rewards; // list of [reward token and total amount]
+    address pooladdr; // which pool this campaign is for
+    address[] tokens; // reward token addr
+    uint256[] amounts; // corresponding amount of reward token
 }
 
-contract Campaign is Ownable {
+contract Campaign is Ownable, Rewards {
     uint64 public constant GRACE_PERIOD = 3600*24*10; // seconds after campaign end
     Config public config;
     IBrevisProof public brvProof;
-    bytes32 public vkHash;
-
-    // user->token cumulative rewards
-    mapping(address => mapping(address => uint256)) public rewards;
-    // user->token already claimed amount
-    mapping(address => mapping(address => uint256)) public claimed;
+    mapping(uint8 => bytes32) public vkMap; // from circuit id to its vkhash
 
     // called by proxy to properly set storage of proxy contract, owner is contract owner (hw or multisig)
-    function init(Config calldata cfg, address owner, IBrevisProof _brv, bytes32 _vkHash) external {
-        initOwner(owner);
+    function init(Config calldata cfg, IBrevisProof _brv) external {
+        initOwner();
+        initTokens(cfg.tokens);
         config = cfg;
         brvProof = _brv;
-        vkHash = _vkHash;
     }
 
     // after grace period, refund all remaining balance to creator
     function refund() external {
         Config memory cfg = config;
         require(block.timestamp>cfg.startTime+cfg.duration+GRACE_PERIOD, "too soon");
-        for (uint256 i=0;i<cfg.rewards.length;i++) {
-            address erc20 = cfg.rewards[i].token;
+        for (uint256 i=0;i<cfg.tokens.length;i++) {
+            address erc20 = cfg.tokens[i];
             IERC20(erc20).transfer(cfg.creator, IERC20(erc20).balanceOf(address(this)));
         }
     }
@@ -56,34 +50,28 @@ contract Campaign is Ownable {
         _claim(msg.sender, to);
     }
 
-    function _claim(address earner, address to) internal {
-        Config memory cfg = config;
-        for (uint256 i=0;i<cfg.rewards.length;i++) {
-            address erc20 = cfg.rewards[i].token;
-            uint256 tosend = rewards[earner][erc20] - claimed[earner][erc20];
-            claimed[earner][erc20] = rewards[earner][erc20];
-            // send token
-            IERC20(erc20).transfer(to, tosend);
-        }
-    }
-
-    // update rewards map w/ zk proof, _appOutput is in the form of [earner:amt u128:amt u128]
+    // update rewards map w/ zk proof, _appOutput is 2(reward app id), t0, t1, [earner:amt u128:amt u128]
     function updateRewards(bytes calldata _proof, bytes calldata _appOutput) external {
-        (, bytes32 appCommitHash, bytes32 appVkHash) = brvProof.submitProof(uint64(block.chainid), _proof);
-        require(appVkHash == vkHash, "mismatch vkhash");
-        require(appCommitHash == keccak256(_appOutput), "invalid circuit output");
-        Config memory cfg = config;
-        uint256 numTokens = cfg.rewards.length;
-        for (uint256 idx = 0; idx < _appOutput.length; idx += 20+16*numTokens) {
-            address earner = address(bytes20(_appOutput[idx:idx+20]));
-            for (uint256 i=0; i < numTokens; i+=1) {
-                uint256 amount = uint128(bytes16(_appOutput[idx+20+16*i:idx+20+16*i+16]));
-                rewards[earner][cfg.rewards[i].token] = amount;
-            }
-        }
+        _checkProof(_proof, _appOutput);
+        addRewards( _appOutput[1:]);
     }
 
-    function setvk(bytes32 _vk) external onlyOwner {
-        vkHash = _vk;
+    // _appOutput is 1(totalfee app id), pooladdr, epoch, t0, t1
+    function updateTotalFee(bytes calldata _proof, bytes calldata _appOutput) external {
+        _checkProof(_proof, _appOutput);
+        address pooladdr = address(bytes20(_appOutput[1:21]));
+        require(pooladdr == config.pooladdr, "mismatch pool addr");
+        updateFee(_appOutput[21:]);
+    }
+
+    function _checkProof(bytes calldata _proof, bytes calldata _appOutput) internal {
+        (, bytes32 appCommitHash, bytes32 appVkHash) = brvProof.submitProof(uint64(block.chainid), _proof);
+        uint8 appid = uint8(_appOutput[0]);
+        require(appVkHash == vkMap[appid], "mismatch vkhash");
+        require(appCommitHash == keccak256(_appOutput), "invalid circuit output");
+    }
+
+    function setVk(uint8 appid, bytes32 _vk) external onlyOwner {
+        vkMap[appid] = _vk;
     }
 }
--- src/TotalFee.sol
+++ src/TotalFee.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.8.20;
 
 contract TotalFee {
-    address public poolAddr; // pool address, set in init, check w/ circuit out
     struct Fee {
         uint128 token0Amt;
         uint128 token1Amt;
@@ -10,18 +9,13 @@ contract TotalFee {
     mapping(uint32 => Fee) public totalFees; // epochNum to totalfee in this epoch
     event TotalFeePosted(uint32 epoch, uint128 token0Amt, uint128 token1Amt);
 
-    function initTotalFee(address _pool) external {
-        poolAddr = _pool;
-    }
-    // only called by inherited contract, raw is app circuit output
-    // epoch, pooladdr, t0fee, t1fee
+    // only called by inherited contract, raw is app circuit output[:20], caller should first check [0:20] is expected pool addr
+    // epoch, t0fee, t1fee
     function updateFee(bytes calldata raw) internal {
-        require(raw.length == 56, "incorrect data length");
+        require(raw.length == 36, "incorrect data length");
         uint32 epoch = uint32(bytes4(raw[0:4]));
-        address pool = address(bytes20(raw[4:24]));
-        uint128 t0fee = uint128(bytes16(raw[24:40]));
-        uint128 t1fee = uint128(bytes16(raw[40:56]));
-        require(pool == poolAddr, "wrong pool addr");
+        uint128 t0fee = uint128(bytes16(raw[4:20]));
+        uint128 t1fee = uint128(bytes16(raw[20:36]));
 
         totalFees[epoch] = Fee(t0fee, t1fee);
         emit TotalFeePosted(epoch, t0fee, t1fee);
