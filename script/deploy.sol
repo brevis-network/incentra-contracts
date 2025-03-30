ba4845c332ccc7ff04b8c24f2ae49888789b715b
Xiaozhou Li <xl@cs.princeton.edu>
2025-03-29 22:46:41 -0700

    fix deploy

--- script/deploy.sol
+++ script/deploy.sol
@@ -2,12 +2,12 @@
 pragma solidity ^0.8.20;
 
 import "forge-std/Script.sol";
-import {Campaign} from "../src/concentrated-liquidity/CampaignCL.sol";
+import {CampaignCL} from "../src/concentrated-liquidity/CampaignCL.sol";
 
 contract Deploy is Script {
     function run() public {
         vm.startBroadcast();
-        Campaign c = new Campaign();
+        CampaignCL c = new CampaignCL();
         console.log("Campaign impl contract deployed at ", address(c));
         vm.stopBroadcast();
     }
