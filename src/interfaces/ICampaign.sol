554ff1fe3a9a421b619870e5d7104e88d0ba553a
Xiaozhou Li <xl@cs.princeton.edu>
2025-03-31 20:25:01 -0700

    update ICampaign interface

--- src/interfaces/ICampaign.sol
+++ src/interfaces/ICampaign.sol
@@ -4,12 +4,19 @@ pragma solidity ^0.8.20;
 // generaic interface for incentra backend to interact with the campaign contract
 // each campaign contract may only implement a subset of the functions
 interface ICampaign {
-    function updateRewards(bytes calldata _proof, bytes calldata _appOutput, uint32 batchIndex) external;
-    function updateTotalFee(bytes calldata _proof, bytes calldata _appOutput) external;
+    function updateRewards(bytes calldata proof, bytes calldata appOutput, uint32 batchIndex) external;
 
-    // cross-chain related
+    function updateTotalFee(bytes calldata proof, bytes calldata appOutput) external;
+
+    function tokenCumulativeRewards(address token) external view returns (uint256);
+
+    // --------- cross-chain related ---------
+
     function startEpoch(uint64 epoch) external;
+
     function startSubRootGen(uint64 epoch) external;
+
     function genSubRoot(uint64 epoch, uint256 nLeaves) external;
+
     function genTopRoot(uint64 epoch) external;
 }
