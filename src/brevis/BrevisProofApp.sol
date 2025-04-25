950275f1bb2c9f8abd6bc7971d963ae7485e3bd0
Michael Zhou <zhoumotongxue008@gmail.com>
2025-04-24 20:00:23 -0700

    Simplify BrevisProofApp with BrevisProof changes

--- src/brevis/BrevisProofApp.sol
+++ src/brevis/BrevisProofApp.sol
@@ -5,14 +5,6 @@ import "./IBrevisProof.sol";
 
 // App that directly interact with the BrevisProof contract interface.
 abstract contract BrevisProofApp {
-    struct ProofData {
-        bytes32 commitHash;
-        bytes32 appCommitHash; // zk-program computing circuit commit hash
-        bytes32 appVkHash; // zk-program computing circuit Verify Key hash
-        bytes32 smtRoot;
-        bytes32 dummyInputCommitment; // zk-program computing circuit dummy input commitment
-    }
-
     IBrevisProof public brevisProof;
 
     uint32 constant PUBLIC_BYTES_START_IDX = 11 * 32; // the first 10 32bytes are groth16 proof (A/B/C/Commitment), the 11th 32bytes is cPub
@@ -21,15 +13,12 @@ abstract contract BrevisProofApp {
         internal
         returns (bytes32 proofId)
     {
-        // TODO: Move this logic to BrevisProof
-        ProofData memory data = _unpackProofData(_proof);
-        proofId = keccak256(abi.encodePacked(data.appVkHash, data.commitHash, data.appCommitHash));
-        // Skip proof submission if already verified by BrevisProof
-        if (brevisProof.proofs(proofId) == bytes32(0)) {
-            (, bytes32 appCommitHash, bytes32 appVkHash) = brevisProof.submitProof(_chainId, _proof);
-            require(appVkHash == _appVkHash, "mismatch vkhash");
-            require(appCommitHash == keccak256(_appOutput), "invalid circuit output");
-        }
+        // BrevisProof will skip verification if already verified.
+        bytes32 appCommitHash;
+        bytes32 appVkHash;
+        (proofId, appCommitHash, appVkHash) = brevisProof.submitProof(_chainId, _proof);
+        require(appVkHash == _appVkHash, "vkHash mismatch");
+        require(appCommitHash == keccak256(_appOutput), "invalid circuit output");
         return proofId;
     }
 
@@ -41,15 +30,5 @@ abstract contract BrevisProofApp {
     ) internal {
         brevisProof.submitAggProof(_chainId, _proofIds, _proofWithPubInputs);
         brevisProof.validateAggProofData(_chainId, _proofDataArray);
-    }
-
-    function _unpackProofData(bytes calldata _proofWithPubInputs) internal pure returns (ProofData memory data) {
-        data.commitHash = bytes32(_proofWithPubInputs[PUBLIC_BYTES_START_IDX:PUBLIC_BYTES_START_IDX + 32]);
-        data.smtRoot = bytes32(_proofWithPubInputs[PUBLIC_BYTES_START_IDX + 32:PUBLIC_BYTES_START_IDX + 2 * 32]);
-        data.appCommitHash =
-            bytes32(_proofWithPubInputs[PUBLIC_BYTES_START_IDX + 2 * 32:PUBLIC_BYTES_START_IDX + 3 * 32]);
-        data.appVkHash = bytes32(_proofWithPubInputs[PUBLIC_BYTES_START_IDX + 3 * 32:PUBLIC_BYTES_START_IDX + 4 * 32]);
-        data.dummyInputCommitment =
-            bytes32(_proofWithPubInputs[PUBLIC_BYTES_START_IDX + 4 * 32:PUBLIC_BYTES_START_IDX + 5 * 32]);
     }
 }
