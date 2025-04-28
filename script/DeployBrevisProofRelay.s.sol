ad45b8c7e46e4393990e6fb9c9415083416de5de
jinyao <1650091775@qq.com>
2025-04-28 14:56:14 +0800

    add brevis proof relay script (#19)
    * add brevis proof relay script
    
    * add claim all script

---
+++ script/DeployBrevisProofRelay.s.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "forge-std/Script.sol";
+import {BrevisProofRelay} from "../src/brevis/BrevisProofRelay.sol";
+import {IBrevisProof} from "../src/brevis/IBrevisProof.sol";
+
+
+contract DeployBrevisProofRelay is Script {
+    function run() public {
+        vm.startBroadcast();
+        BrevisProofRelay c = new BrevisProofRelay(IBrevisProof(address(0)));
+        console.log("BrevisProofRelay contract deployed at ", address(c));
+        vm.stopBroadcast();
+    }
+}
