de1bb50d01803452bfd6259585097dd523aaf1b5
Xiaozhou Li <xli@celer.network>
2025-05-27 22:53:10 -0700

    support external payout (#25)
    * add externalPayoutAddress
    
    * update comment

--- src/rewards/same-chain/RewardsClaim.sol
+++ src/rewards/same-chain/RewardsClaim.sol
@@ -20,19 +20,22 @@ abstract contract RewardsClaim is RewardsStorage {
     // seconds after campaign end, after which all remaining balance can be refunded to creator
     uint64 public gracePeriod = 3600 * 24 * 180; // default 180 days
 
+    // If set, only this address can call claim; rewards are not transferred directly, external address handles payout.
+    address public externalPayoutAddress;
+
     event RewardsClaimed(address indexed earner, uint256[] claimedRewards);
     event GracePeriodUpdated(uint64 gracePeriod);
 
     // ----- external functions -----
 
     // claim reward, send erc20 to earner
-    function claim(address earner) external {
-        _claim(earner, earner);
+    function claim(address earner) external returns (address[] memory, uint256[] memory) {
+        return _claim(earner, earner);
     }
 
     // msg.sender is the earner
-    function claimWithRecipient(address to) external {
-        _claim(msg.sender, to);
+    function claimWithRecipient(address to) external returns (address[] memory, uint256[] memory) {
+        return _claim(msg.sender, to);
     }
 
     function viewUnclaimedRewards(address earner) external view returns (AddrAmt[] memory) {
@@ -52,7 +55,11 @@ abstract contract RewardsClaim is RewardsStorage {
 
     // ----- internal functions -----
 
-    function _claim(address earner, address to) internal {
+    function _setExternalPayoutAddress(address _externalPayoutAddress) internal {
+        externalPayoutAddress = _externalPayoutAddress;
+    }
+
+    function _claim(address earner, address to) internal returns (address[] memory, uint256[] memory) {
         uint256[] memory claimedRewards = new uint256[](tokens.length);
         bool hasUnclaimed = false;
         for (uint256 i = 0; i < tokens.length; i++) {
@@ -62,7 +69,11 @@ abstract contract RewardsClaim is RewardsStorage {
             claimed[earner][token] = cumulativeAmount;
             // send token
             if (tosend > 0) {
-                IERC20(token).safeTransfer(to, tosend);
+                if (externalPayoutAddress == address(0)) {
+                    IERC20(token).safeTransfer(to, tosend);
+                } else {
+                    require(msg.sender == externalPayoutAddress, "unauthorized caller");
+                }
                 tokenClaimedRewards[token] += tosend;
                 hasUnclaimed = true;
             }
@@ -70,5 +81,6 @@ abstract contract RewardsClaim is RewardsStorage {
         }
         require(hasUnclaimed, "no unclaimed rewards");
         emit RewardsClaimed(earner, claimedRewards);
+        return (tokens, claimedRewards);
     }
 }
