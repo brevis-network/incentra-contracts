cdd5959a7c1b0171e2fabcc545d72c6880ad17d8
Michael Zhou <zhoumotongxue008@gmail.com>
2025-12-11 23:43:14 -0800

    Fix to take claimed amount into account

--- src/rewards/cross-chain/CampaignRewardsClaim.sol
+++ src/rewards/cross-chain/CampaignRewardsClaim.sol
@@ -228,7 +228,7 @@ contract CampaignRewardsClaim is AccessControl, MessageReceiverApp {
 
     /**
      * @notice Allows the owner to recover any non-reward ERC20 tokens mistakenly sent to this contract
-     *    or reward tokens in excess of the configured amount.
+     *    or reward tokens in excess of (configured amount - claimed amount).
      * @param _erc20 Address of the ERC20 token to recover
      * @param _to Address to send the recovered tokens to
      * @param _amount Amount of tokens to recover
@@ -239,12 +239,15 @@ contract CampaignRewardsClaim is AccessControl, MessageReceiverApp {
         uint256 balance = _erc20.balanceOf(address(this));
         require(_amount <= balance, "insufficient balance");
 
-        for (uint256 i = 0; i < config.rewards.length; i++) {
-            AddrAmt memory reward = config.rewards[i];
-            require(
-                (address(_erc20) != reward.token) || (_amount <= (balance - reward.amount)),
-                "cannot recover reserved reward token"
-            );
+        if (config.externalPayoutAddress == address(0)) {
+            for (uint256 i = 0; i < config.rewards.length; i++) {
+                AddrAmt memory reward = config.rewards[i];
+                require(
+                    (address(_erc20) != reward.token)
+                        || (_amount <= (balance - (reward.amount - tokenClaimedRewards[reward.token]))),
+                    "cannot recover reserved reward token"
+                );
+            }
         }
 
         _erc20.safeTransfer(_to, _amount);
